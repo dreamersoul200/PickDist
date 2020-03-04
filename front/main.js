0d568df4c7921c8ed13273eb186f45fed9355a69
Angel <angel.huamani.e@hotmail.com>
2020-03-04 11:31:36 -0500

    Initial marker added

--- front/main.js
+++ front/main.js
@@ -21,4 +21,44 @@ function initMap() {
         },
         zoom: 15
     });
+
+    const drawingManager = new google.maps.drawing.DrawingManager({
+        drawingMode: google.maps.drawing.OverlayType.MARKER,
+        drawingControl: false
+    });
+    //drawingManager.setMap(map);
+
+    /*
+    google.maps.event.addListener(drawingManager, 'markercomplete', function(marker) {
+        //Devuelve un marker
+    });
+    */
+
+    const marker1 = new google.maps.Marker({
+        draggable: true,
+        icon: './marker.png',
+        map: map,
+        position: {
+            lat: -12.026094,
+            lng: -77.061433
+        }
+    });
+
+    const geocoder = new google.maps.Geocoder;
+    /*
+    geocoder.geocode({
+        location: {
+            lat: -12.026094,
+            lng: -77.061433
+        }
+    });
+    */
+
+    /*
+    google.maps.event.addListener(marker1, 'dragend', () => {
+        const position = marker1.getPosition();
+        console.log(position.lat(), position.lng());
+    });
+    */
+
 }
